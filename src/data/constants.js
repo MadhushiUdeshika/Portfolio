83b868bd6288638ff8a3456369912ee355d9f98f
Madhushi Udeshika <pkmudeshika@gmail.com>
2024-01-29 22:01:29 +0530

    Update

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,8 +2,7 @@ export const Bio = {
     name: "Madhushi Udeshika",
     roles: [
       "AI/ML Enthusiast",
-      "Frontend Developer",
-      "Web & Mobile App Developer"
+      "Frontend Developer"
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
